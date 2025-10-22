567c8da8e177a0b5f7c9b1b6430bc6c3057ec15b
Ola Viken <olaviken@hotmail.com>
2025-10-22 21:11:29 +0200

    Started building the global class educator. This is supposed to replace the local class at addEducator.

--- Components/Classes/Educator.cs
+++ Components/Classes/Educator.cs
@@ -1,6 +1,20 @@
-﻿namespace BlazorTest.Components.Classes
+﻿using System;
+
+namespace BlazorTest.Components.Classes
 {
-    public class Educator
+    class Educator
     {
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public DateTime BirthDate { get; private set; }
+        public string Email { get; private set; } = string.Empty;
+        public string Position { get; private set; } = string.Empty;
+        public string SubjectUnit { get; private set; } = string.Empty;
+        public string Education { get; private set; } = string.Empty;
+        public string Specialization { get; private set; } = string.Empty;
+
+        public string UserName { get; private set; } = string.Empty;
+        public int Age { get; private set; }
+        public int HoursForEducation { get; private set; }
     }
 }

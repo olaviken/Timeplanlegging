525c7de9b83e1454d7f95c537e78395b3809e193
Ola Viken <olaviken@hotmail.com>
2025-10-26 14:26:22 +0100

    Added a update methods

--- Components/Classes/EducatorList.cs
+++ Components/Classes/EducatorList.cs
@@ -1,9 +1,56 @@
-﻿namespace BlazorTest.Components.Classes
+﻿using System.Data.Common;
+
+namespace BlazorTest.Components.Classes
 {
 
     public class EducatorList
     {
-        public List<Educator> educators = new List<Educator>();
+        public List<Educator> Educators { get; private set; } = new List<Educator>();
+
+
+
+        public EducatorList() { }
+        public void AddEducator(Educator educator)
+        {
+            if (educator == null)
+            {
+                throw new ArgumentNullException(nameof(educator), "Educator cannot be null.");
+            }
+            Educators.Add(educator);
+        }
+        
+        public void RemoveEducator(Educator educator)
+        {
+            if (educator == null)
+            {
+                throw new ArgumentNullException(nameof(educator), "Educator cannot be null.");
+            }
+            Educators.Remove(educator);
+        }
+
+        public Educator? FindEducatorByEmail(string email)
+        {
+            return Educators.FirstOrDefault(e => e.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void UpdateEducator(Educator updatedEducator)
+        {
+            if (updatedEducator == null)
+            {
+                throw new ArgumentNullException(nameof(updatedEducator), "Updated educator cannot be null.");
+            }
+            var existingEducator = FindEducatorByEmail(updatedEducator.Email);
+            if (existingEducator != null)
+            {
+                int index = Educators.IndexOf(existingEducator);
+                Educators[index] = updatedEducator;
+            }
+            else
+            {
+                throw new InvalidOperationException("Educator not found.");
+            }
+        }
+
     }
         
 }

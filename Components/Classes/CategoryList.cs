038e7d011350fcd03aa5af58e0fd12efc60b14bf
Ola Viken <olaviken@hotmail.com>
2025-10-29 21:34:57 +0100

    Added add category to list

--- Components/Classes/CategoryList.cs
+++ Components/Classes/CategoryList.cs
@@ -3,5 +3,20 @@
     public class CategoryList
     {
         public List<EducatioCategory> EducationCategories { get; private set; } = new();
+
+        public CategoryList() { }
+
+        public void AddCategory(EducatioCategory category)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException(nameof(category), "Category cannot be null.");
+            }
+            if (EducationCategories.Any(c => c.CategoryName == category.CategoryName))
+            {
+                throw new InvalidOperationException($"Category with name '{category.CategoryName}' already exists.");
+            }
+            EducationCategories.Add(category);
+        }
     }
 }

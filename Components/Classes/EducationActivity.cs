5e8972e1f56863b55f65cf33918039a37243ebf8
Ola Viken <olaviken@hotmail.com>
2025-11-11 23:04:43 +0100

    added variables to the education activity. reworked the categories so that it is possible to add or update a single variable. Made sure that hours for education fall and spring is showing properly in the Listing Educator

--- Components/Classes/EducationActivity.cs
+++ Components/Classes/EducationActivity.cs
@@ -2,5 +2,14 @@
 {
     public class EducationActivity
     {
+        public DateTime ActivityDate { get; private set; }
+        public string ActivityName { get; private set; } = string.Empty;
+        public float ActivityHours { get; private set; } = 0f;
+        public EducationCategory? ActivityCategory { get; private set; } = null;
+        public string ActivityDescription { get; private set; } = string.Empty;
+        public string EducatorFirstName { get; private set; } = string.Empty;
+        public string EducatorLastName { get; private set; } = string.Empty;
+
+
     }
 }

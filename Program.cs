ec473e214fddb60370e82e5bf82def530a7aa4bb
Ola Viken <olaviken@hotmail.com>
2025-11-26 20:07:47 +0100

    Modified program to start implementing in memory DB

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
+builder.Services.AddInMemoryDatabase();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {

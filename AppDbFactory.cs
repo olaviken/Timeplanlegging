827905df9491129ff29b334495b7e52cbc7e6a57
Ola Viken <olaviken@hotmail.com>
2025-11-26 20:07:22 +0100

    Added a static class for handling the initialization of in memorydb

---
+++ AppDbFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorTest
+{
+    public static class AppDbFactory
+    {
+        public static void AddInMemoryDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("RessursPlan"));
+        }
+    }
+}

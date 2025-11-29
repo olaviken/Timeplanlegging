251de87c1bd52308ee6065041c3aa91b297e540e
Ola Viken <olaviken@hotmail.com>
2025-11-29 22:58:06 +0100

    Removed Activity class it was redundant and duplicate to EducationActivity. Added singleton services in program for all three lists.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using BlazorTest.Components;
+using BlazorTest.Components.Classes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Added Singleton Services
+builder.Services.AddSingleton<ActivityList>();
+builder.Services.AddSingleton<EducatorList>();
+builder.Services.AddSingleton<CategoryList>();
 
 var app = builder.Build();
 

f04a2dffc2afaa47650f70311ec646edbb8dd216
Ola Viken <olaviken@hotmail.com>
2025-12-15 20:24:41 +0100

    Created a class subjects

---
+++ Components/Classes/Subject.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTest.Components.Classes
+{
+    public class Subject
+    {
+        public string SubjectCode { get; set; } = string.Empty;
+        public string SubjectName { get; set; }= string.Empty;
+        public string SubjectDescription { get; set; }= string.Empty;
+    }
+}

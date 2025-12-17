0b4a09dd78d19a8ebedeb588370c148e103f026c
Ola Viken <olaviken@hotmail.com>
2025-12-17 21:06:26 +0100

    Created an interface

---
+++ Components/Classes/IFields.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorTest.Components.Classes
+{
+    public interface IFields
+    {
+        List<Field> GetFields();
+
+        void AddField(Field field);
+        void RemoveField(Field field);
+        Field? FindFieldByID(string fieldID);
+        void UpdateField(Field updatedField);
+        void DeleteField(Field field);
+
+
+    }
+}

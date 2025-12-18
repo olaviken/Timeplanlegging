fb55572a4333d74f1f172367653baa611c78b0bd
Ola Viken <olaviken@hotmail.com>
2025-12-18 23:21:18 +0100

    Created an interface for activities

---
+++ Components/Classes/IActivities.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorTest.Components.Classes
+{
+    public interface IActivities
+    {
+        List<Activity> GetActivities();
+        void AddActivity(Activity activity);
+        void RemoveActivity(Activity activity);
+        Activity? FindActivityByID(string activityID);
+        void UpdateActivity(Activity updatedActivity);
+        int CountActivities();
+    }
+}

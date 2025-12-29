01ce538865bd393ee5a32672d164b69913566dab
Ola Viken <olaviken@hotmail.com>
2025-12-29 21:33:29 +0100

    Created interface ISubjects

--- Components/Classes/Interfaces/ISubjects.cs
+++ Components/Classes/Interfaces/ISubjects.cs
@@ -2,6 +2,11 @@
 {
     public interface ISubjects
     {
-
+        List<Subject> GetSubjects();
+        void AddSubject(Subject subject);
+        void RemoveSubject(Subject subject);
+        Subject? FindSubjectByCode(string subjectCode);
+        void UpdateSubject(Subject updatedSubject);
+        int CountSubjects();
     }
 }

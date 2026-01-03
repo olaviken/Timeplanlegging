3f74719532cf0e0eb3217e2fea28dd0361d660cd
Ola Viken <olaviken@hotmail.com>
2026-01-03 18:04:10 +0100

    Made use of test class of educational categories

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorComponents()
 //Added Singleton Services
 builder.Services.AddSingleton<IActivities, ListActivity>();
 builder.Services.AddSingleton<IEducators, ListEducator>();
-builder.Services.AddSingleton<ICategories, ListCategory>();
+builder.Services.AddSingleton<ICategories, ListCategoryTest>();
 builder.Services.AddSingleton<IFields, ListField>();
 builder.Services.AddSingleton<ISubjects, ListSubject>();
 

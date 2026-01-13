df0ea69061752940f87c0aa9b960eabec8be33a4
Ola Viken <olaviken@hotmail.com>
2026-01-13 23:17:14 +0100

    Added test class educators to singleton

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddRazorComponents()
 
 //Added Singleton Services
 builder.Services.AddSingleton<IActivities, ListActivity>();
-builder.Services.AddSingleton<IEducators, ListEducator>();
+builder.Services.AddSingleton<IEducators, ListEducatorsTest>();
 builder.Services.AddSingleton<ICategories, ListCategoryTest>();
 builder.Services.AddSingleton<IFields, ListFieldsTest>();
 builder.Services.AddSingleton<ISubjects, ListSubjectsTest>();

dba3c7a3d242f7f4918a94ff471e06b783719b7c
Ola Viken <olaviken@hotmail.com>
2026-01-26 21:55:57 +0100

    Created services for the selected items

---
+++ Components/Services/SelectedFieldService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BlazorTest.Components.Classes;
+
+
+namespace BlazorTest.Components.Services
+{
+    
+    public class SelectedFieldService
+    {
+        public Field? Current { get; private set; }
+        public event Action? OnChanged;
+
+        public void Set(Field field)
+        {
+            Current = field;
+            OnChanged?.Invoke();
+        }
+    }
+}
---
+++ Components/Services/SelectedSubjectService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorTest.Components.Classes;
+
+namespace BlazorTest.Components.Services
+{
+    public class SelectedSubjectService
+    {
+        public Subject? Current { get; private set; }
+        public event Action? OnChanged;
+        public void Set(Subject subject)
+        {
+            Current = subject;
+            OnChanged?.Invoke();
+        }
+    }
+}

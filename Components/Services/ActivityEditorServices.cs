f671a87a31a6e3a5aa2dfaaacd0d0f62f7682045
Ola Viken <olaviken@hotmail.com>
2026-01-31 16:52:19 +0100

    Created an Activity editor services that is used across all pages that lists activites

---
+++ Components/Services/ActivityEditorServices.cs
@@ -0,0 +1,54 @@
+﻿using BlazorTest.Components.Classes;
+using BlazorTest.Components.Classes.Interfaces;
+
+namespace BlazorTest.Components.Services
+{
+    public class ActivityEditorServices : IActivityEditorService
+    {
+        private readonly IActivities _activities;
+
+        public Activity Current { get; private set; }
+
+        public void Reset()
+        {
+            Current = new Activity{ActivityDate = DateTime.Today};
+        }
+
+        public void StartEdit(Activity activity)
+        {
+            if (activity != null) Current = activity;
+        }
+
+
+        public void AddOrUpdate()
+        {
+            if (Current == null) return;
+            if((!string.IsNullOrEmpty(Current.ActivityID)) && (_activities.FindActivityByID(Current.ActivityID) != null))
+            {
+                _activities.UpdateActivity(Current);
+
+            }
+            else 
+            {
+                _activities.AddActivity(Current);
+            }
+            Reset();
+
+        }
+        public void Delete(Activity activity)
+        {
+            if(activity != null)    
+            {
+                _activities.RemoveActivity(activity);
+            }
+        }
+
+        public void UpdateCalculatedHours(float multiplier)
+        {
+            if(Current != null)
+            {
+                Current.CalculatedHours = Current.ActivityHours * multiplier;
+            }
+        }
+    }
+}

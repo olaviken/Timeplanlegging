9772443710e84995a091d29715784c7245ec5bb9
Ola Viken <olaviken@hotmail.com>
2026-03-01 21:58:42 +0100

    Added errormessage and required fields

--- Components/Classes/Activity.cs
+++ Components/Classes/Activity.cs
@@ -1,21 +1,32 @@
 ﻿using BlazorTest.Components.Classes.Interfaces;
 using BlazorTest.Components.Classes.Lists;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorTest.Components.Classes
 {
     public class Activity
     {
         public string ActivityID { get; set; } = Guid.NewGuid().ToString();
+
+        [Required(ErrorMessage = "Activity date is required.")]
         public DateTime ActivityDate { get; set; }
+
+        [Required(ErrorMessage = "Activity name is required.")]
         public string ActivityName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Activity hours are required.")]
         public float ActivityHours { get; set; } = 0f;
 
         public float CalculatedHours { get; set; } = 0f;
         public string ActivityCategory { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Subject code is required.")]
         public string SubjectCode { get; set; } = string.Empty;
         public string FieldTitle { get; set; } = string.Empty;
 
         public string ActivityDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Educator full name is required.")]
         public string EducatorFullName { get; set; } = string.Empty;
 
     }
--- Components/Classes/EducationCategory.cs
+++ Components/Classes/EducationCategory.cs
@@ -1,9 +1,16 @@
-﻿namespace BlazorTest.Components.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorTest.Components.Classes
 {
     public class EducationCategory
     {
+        [Required(ErrorMessage ="Category name is required")]
         public string CategoryName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Hours multiplier is required")]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Hours multiplier must be greater than 0")]
         public float HoursMultiplier { get; set; } = 1;
 
     }  

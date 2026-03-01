213dbddab2940744a365645f2ebec4d9d4f05587
Ola Viken <olaviken@hotmail.com>
2026-03-01 22:00:14 +0100

    Added errormessage and required fields

--- Components/Classes/Subject.cs
+++ Components/Classes/Subject.cs
@@ -1,8 +1,13 @@
-﻿namespace BlazorTest.Components.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTest.Components.Classes
 {
     public class Subject
     {
+        [Required(ErrorMessage = "Subject code is required.")]
         public string SubjectCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Subject name is required.")]
         public string SubjectName { get; set; }= string.Empty;
         public string SubjectDescription { get; set; }= string.Empty;
     }
